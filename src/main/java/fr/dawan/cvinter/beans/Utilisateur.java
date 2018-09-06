851f00c082bead56a8661c9f91aeb43ed348c119
geoffrey <geoffrey.vanacker@gmail.com>
2018-09-06 11:15:26 +0200

    Test push GV après push de MV

--- src/main/java/fr/dawan/cvinter/beans/Utilisateur.java
+++ src/main/java/fr/dawan/cvinter/beans/Utilisateur.java
@@ -2,4 +2,41 @@ package fr.dawan.cvinter.beans;
 
 public class Utilisateur {
 
+	
+	private String Prenom;
+	private String Nom;
+	private String Email;
+	private String mdp;
+
+	public String getPrenom() {
+		return Prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		Prenom = prenom;
+	}
+
+	public String getNom() {
+		return Nom;
+	}
+
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
 }
