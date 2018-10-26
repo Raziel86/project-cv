e288bfa001c215cedbab506a4b95b02b31ae2452
geoffrey <geoffrey.vanacker@gmail.com>
2018-10-26 14:23:29 +0200

    authentication rajoutée

---
+++ src/main/java/fr/dawan/cvinter/dao/AuthenticationDao.java
@@ -0,0 +1,22 @@
+package fr.dawan.cvinter.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import fr.dawan.cvinter.beans.Login;
+import fr.dawan.cvinter.beans.User;
+
+public class AuthenticationDao {
+
+	
+	public static User findUser(Login login, EntityManager em, boolean closeConnection) {
+		Query query = em.createQuery("FROM user u WHERE u.email = ? and u.mpd = ?").setParameter(1, login.getEmail()).setParameter(2, login.getPassword());
+		User user = (User) query.getSingleResult();
+		
+		if (closeConnection)
+			em.close();
+		
+		return user;	
+	}
+	
+}
